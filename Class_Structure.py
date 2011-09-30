e8c71c6d7874ef66672629f0418b5b8c170eb3eb
Cameron Neylon <cn@cameronneylon.net>
2011-09-30 13:24:01 +0100

    Some modifications to take output away from stdout

--- Class_Structure.py
+++ Class_Structure.py
@@ -2,38 +2,43 @@ import subprocess
 import os.path
 
 class Structure:
-    
-    def __init__(self,pdb=None):
-        
-        self.pdb=pdb
+
+    def __init__(self, pdb=None):
+        """Init method for a toy structure class"""
+
+        self.pdb = pdb
         self.pdbpath = self.pdb + '.pdb'
 
     def runCrysol(self):
-        
-        if os.path.exists(self.pdbpath):
-            subprocess.Popen(['crysol',self.pdbpath])
-            return
-        else:
-            print 'diSASTER!'
+        """Method for running crysol over the pdb""" 
 
-    def runCryson(self, d2o=1.0):
-        d2o=str(d2o)
         if os.path.exists(self.pdbpath):
-            p=subprocess.Popen(['cryson',self.pdbpath,'/d2o',d2o])
+            p = subprocess.Popen(['crysol', self.pdbpath], stdout=subprocess.PIPE)
             p.wait()
             return
+        
         else:
-            print 'diSASTER!'
+            print "Can't find file"
 
-pdb=raw_input('pdb code?  ')
-p=Structure(pdb)
+    def runCryson(self, d2o=1.0):
+        """Method for running crysol over the pdb"""
+
+        d2o = str(d2o)
+        if os.path.exists(self.pdbpath):
+            p = subprocess.Popen(['cryson', self.pdbpath, '/d2o', d2o], stdout=subprocess.PIPE)
+            p.wait()
+            return
+        
+        else:
+            print "Can't find file"
+
+
+
+p = Structure('fen1')
 p.runCrysol()
 p.runCryson()
-x=0.0
-while x<=1.0:
-    p.runCryson(x)
-    x=x+0.1
+p.runCryson(0.0)
 
-    
-
-                
+        
+        
+        
